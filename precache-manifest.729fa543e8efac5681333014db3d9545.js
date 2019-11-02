a3e138d9ea3b28f55032653edb4c1ecf20e41756
NikitaKlepec <clepts.nikita@gmail.com>
2019-11-02 17:45:01 +0300

    Updates

---
+++ precache-manifest.729fa543e8efac5681333014db3d9545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58eb3728919ac5a821694ad693a3c9a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "44e05aa597fb4ab86b03",
+    "url": "/Portfolio/static/css/main.68892bf9.chunk.css"
+  },
+  {
+    "revision": "9a31fd4ee1caefd4abcc",
+    "url": "/Portfolio/static/js/2.d26cbee5.chunk.js"
+  },
+  {
+    "revision": "44e05aa597fb4ab86b03",
+    "url": "/Portfolio/static/js/main.d7029201.chunk.js"
+  },
+  {
+    "revision": "d445793bb5cd0d3feaec",
+    "url": "/Portfolio/static/js/runtime-main.020f363e.js"
+  }
+]);
